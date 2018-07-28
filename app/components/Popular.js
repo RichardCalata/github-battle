dbee4c0886e87a6f4bfcb135f8ab5e02fae2e2ab
richardcalata <calatar6@gmail.com>
2018-07-28 16:33:38 -0500

    47% complete

---
+++ app/components/Popular.js
@@ -0,0 +1,38 @@
+var React = require('react');
+
+class Popular extends React.Component{
+    constructor(props){
+        super(props);
+        this.state ={
+            selectedLanguage:'All'
+        };
+        this.updateLanguage = this.updateLanguage.bind(this);
+    }
+
+    updateLanguage(lang){
+        this.setState(function(){
+            return {
+                selectedLanguage: lang
+            }
+        });
+    }
+    render(){
+
+        var languages = ['All','Javascript','Ruby','Java','CSS','Python'];
+        console.log('Up here', this);
+        return(
+            <ul className='languages'>
+                {languages.map(function(lang){
+                    console.log('down here',this);
+                    return (
+                    <li key={lang}>
+                        {lang}
+                    </li>
+                )
+                })}
+            </ul>
+            )
+    }
+}
+
+module.exports = Popular;
--- app/index.js
+++ app/index.js
@@ -1,15 +1,11 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var PropTypes = require('prop-types');
 require('./index.css');
-//state
-//lifecycle event
-//UI
-class App extends React.Component {
-    render(){
-        return(
-        <div>
-            Hello World!
-        </div>
-        )
-    }
-}
+
+var App = require('./components/App');
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
